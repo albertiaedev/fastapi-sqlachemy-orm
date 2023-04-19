e48d9bd1f80033ff04bb130128ce3296f72f323a
Alberto <albertiae.digital@gmail.com>
2023-04-19 14:51:04 -0400

    Create model attributes/columns and the relationship between them

--- sql_app/models.py
+++ sql_app/models.py
@@ -4,3 +4,25 @@ from sqlalchemy.orm import relationship
 
 # Import Base from database.py
 from .database import Base
+
+# Create model attributes/columns and the relationship between them
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    items = relationship("Item", back_populates="owner")
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="items")
