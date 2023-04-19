4af611c1fc6ca38f9ffd4c698e3118ee244225b4
Alberto <albertiae.digital@gmail.com>
2023-04-19 14:39:47 -0400

    Create a SessionLocal class

--- sql_app/database.py
+++ sql_app/database.py
@@ -13,3 +13,6 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+# Create a SessionLocal class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
