8a65395bd5fe4aca9179dd5b547f845666ee2c80
Alberto <albertiae.digital@gmail.com>
2023-04-19 14:48:02 -0400

    Import Base from database.py

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,3 +1,6 @@
 # Importing from SQLAlachemy
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+
+# Import Base from database.py
+from .database import Base
