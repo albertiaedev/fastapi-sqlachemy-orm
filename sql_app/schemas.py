eb8fba34b74d229ebc5b0ae05d9ad09eaf232251
Alberto <albertiae.digital@gmail.com>
2023-04-19 17:55:18 -0400

    Create initial Pydantic models/schemas using orm_mode

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,4 +1,36 @@
 # Importing from Pydantic
 from pydantic import BaseModel
 
+# Create initial Pydantic models/schemas using orm_mode
+class ItemBase(BaseModel):
+    title: str
+    description: str | None = None
 
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: list[Item] = []
+
+    class Config:
+        orm_mode = True
