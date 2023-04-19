370d4f4835cbab420a01bed16661b779c0c9a599
Alberto <albertiae.digital@gmail.com>
2023-04-19 14:35:57 -0400

    Create a database URL for SQLAlchemy

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,3 +2,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+# Create a database URL for SQLAlchemy
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# In this app we're  using SQLite
+# If you were using a PostgreSQL database instead, you would just have to uncomment the line:
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
