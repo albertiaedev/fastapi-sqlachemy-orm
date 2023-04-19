c6e342e06cf23c9fdd45b8748f8d76c2a662b613
Alberto <albertiae.digital@gmail.com>
2023-04-19 14:38:09 -0400

    Create the SQLAlchemy engine

--- sql_app/database.py
+++ sql_app/database.py
@@ -8,3 +8,8 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # In this app we're  using SQLite
 # If you were using a PostgreSQL database instead, you would just have to uncomment the line:
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+# Create the SQLAlchemy engine
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
