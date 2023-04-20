f93633abd35cfaac760051ea35c906c953f333be
Alberto <albertiae.digital@gmail.com>
2023-04-19 21:10:18 -0400

    Create the crud functions

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -4,3 +4,35 @@ from sqlalchemy.orm import Session
 # Import models from SQLAlchemy models & schemas from Pydantic schemas
 from . import models, schemas
 
+# READ function
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+# READ function
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+# READ function
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+# CREATE function
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+# READ function
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+# CREATE function
+def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+    db_item = models.Item(**item.dict(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
